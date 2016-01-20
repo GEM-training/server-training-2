e5794f78c3244ec25a8a4e3295ca5c4bd3edba67
hoapham <hoak57uet@gmail.com>
2016-01-20 17:20:11 +0700

    +add ProductDao

---
+++ src/main/java/Dao/ProductDaoImpl.java
@@ -0,0 +1,45 @@
+package Dao;
+
+import Models.Dealer;
+import Models.Make;
+import Models.Product;
+import Models.Sale;
+import org.hibernate.Criteria;
+
+import java.util.List;
+
+/**
+ * Created by hoapham on 20/01/2016.
+ */
+public class ProductDaoImpl extends AbstractDao implements ProductDao {
+
+    public List<Product> getAllProducts() {
+        Criteria criteria = getSession().createCriteria(Product.class);
+        return (List<Product>) criteria.list();
+    }
+
+    public Product findById(Integer productId) {
+         return (Product)getObjectById(productId);
+    }
+
+    public Integer save(Product product) {
+        return (Integer)saveObject(product);
+    }
+
+    public void delete(Integer productId) {
+        delete(productId);
+    }
+
+    public List<Dealer> getDealers(Integer productId) {
+        return (List<Dealer>)findById(productId).getListDealers();
+    }
+
+    public Make getMake(Integer productId) {
+        return findById(productId).getMake();
+    }
+
+    public List<Sale> getListSales(Integer productId) {
+        return (List<Sale>)findById(productId).getListSales();
+    }
+
+}
