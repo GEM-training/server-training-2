70640d23ecf1b79721463cc371c6024c2bc6e347
phongnt57 <phongnt.9441@gmail.com>
2016-01-21 09:20:24 +0700

    remove .class file

--- src/main/java/Controllers/ProductDealerController.java
+++ src/main/java/Controllers/ProductDealerController.java
@@ -1,7 +1,18 @@
 package Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by phong on 21/01/2016.
  */
+@RestController
+@RequestMapping("/productdealer")
+@Transactional
+
 public class ProductDealerController {
+
+
 }
--- src/main/java/Dao/ProductDealerDao.java
+++ src/main/java/Dao/ProductDealerDao.java
@@ -9,9 +9,9 @@ import java.util.List;
 /**
  * Created by phong on 19/01/2016.
  */
-public interface Product_dealerDao {
-    public List<ProductDealer> getAllProduct();
+public interface ProductDealerDao {
+    public List<ProductDealer> getAllProductDeals();
     public Integer save(ProductDealer product_dealer);
-    public void deleteProduct_dealer(Integer id);
+    public void deleteProductDealer(Integer id);
     public ProductDealer findById(Integer id);
 }
