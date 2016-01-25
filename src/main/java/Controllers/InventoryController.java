e49d0a31c93cb1726c986ae29f6308fcf8ef3216
nguyen <nguyenpk_57@vnu.edu.vn>
2016-01-25 08:10:54 +0700

    Merge branch 'master' of https://github.com/GEM-training/server-training-2
    Conflicts:
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Controllers/ProductController.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Controllers/ProductDealerController.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/DealerDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/DealersDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/InventoryDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/InventoryDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/MakeDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/ProductDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/ProductDealerDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/ProductDealerDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/ProductInventoryDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/ProductInventoryDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/SaleDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/SaleDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/SaleDetailDao.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/SaleDetailDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Dao/StaffDaoImpl.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Models/ProductDealer.class
    	out/artifacts/ServiceTeam2_war_exploded/WEB-INF/classes/Models/ProductInventory.class
    	src/main/java/Controllers/ProductInventoryController.java
    	target/classes/Controllers/ProductController.class
    	target/classes/Controllers/ProductDealerController.class
    	target/classes/Dao/CustomerDao.class
    	target/classes/Dao/CustomerDaoImpl.class
    	target/classes/Dao/DealerDaoImpl.class
    	target/classes/Dao/DealersDao.class
    	target/classes/Dao/InventoryDao.class
    	target/classes/Dao/InventoryDaoImpl.class
    	target/classes/Dao/MakeDao.class
    	target/classes/Dao/MakeDaoImpl.class
    	target/classes/Dao/ProductDaoImpl.class
    	target/classes/Dao/ProductDealerDao.class
    	target/classes/Dao/ProductDealerDaoImpl.class
    	target/classes/Dao/ProductInventoryDao.class
    	target/classes/Dao/ProductInventoryDaoImpl.class
    	target/classes/Dao/SaleDao.class
    	target/classes/Dao/SaleDaoImpl.class
    	target/classes/Dao/SaleDetailDao.class
    	target/classes/Dao/SaleDetailDaoImpl.class
    	target/classes/Dao/StaffDaoImpl.class
    	target/classes/Models/ProductDealer.class
    	target/classes/Models/ProductInventory.class
    	target/classes/Models/Sale.class
    	target/classes/Models/SaleDetail.class
    	target/classes/Models/Staff.class

--- src/main/java/Controllers/InventoryController.java
+++ src/main/java/Controllers/InventoryController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 /**
  * Created by nguyenkhoi on 1/21/16.
